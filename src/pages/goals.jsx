948eadd8dda4e6bd76997fe2011c9ba005ee75af
azizaxampl <you@example.com>
2025-08-25 18:06:01 +0700

    pembaharuan

---
+++ src/pages/goals.jsx
@@ -0,0 +1,20 @@
+function Goals() {
+  return (
+    <section className="card">
+      <div className="about-content">
+        <img src="src/assets/pacarku.jpeg" alt="pacarku" className="profile-img" />
+        <div className="text">
+          <h2>Hal yang ingin aku coba</h2>
+          <ul>
+            <li>Travelling ke luar kota atau pulau</li>
+            <li>Ingin menjadi konten kreator</li>
+            <li>Public speaking di depan banyak orang</li>
+            <li>Saya suka mie ayam dan Iqbal Ramadhan</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Goals
---
+++ src/pages/home.jsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <section className="card">
+      <div className="about-content">
+        <img src="src/assets/foto1.jpeg" alt="yoi" className="profile-img" />
+        <div className="text">
+          <h2>Tentang saya</h2>
+          <p>
+            Saya adalah seorang pelajar di SMK,<br />
+            dan mengambil jurusan "Rekayasa Perangkat Lunak"
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
